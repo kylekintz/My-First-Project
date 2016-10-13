ee787d588389209515363127b7b59852ab440ab4
kylekintz <32999@bwls.net>
2016-10-13 15:12:01 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
- createCanvas( 800 , 800 ); fill( 244 , 66 , 75 );
+ createCanvas( 800 , 800 ); fill( 255 , 0 , 0 );
 }
 
 function draw() {
-  background( 238 , 234 , 66 ); ellipse( mouseX , mouseY , 80 , 80 );
+  ellipse( mouseX , mouseY , 80 , 80 );
 }
  
